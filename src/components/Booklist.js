375fa587acc2473a7af114e7c7637dec2d24a7c3
meghnavarma0 <meghnavarma0@gmail.com>
2020-06-19 01:36:22 +0530

    basic layout

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+	render() {
+		return (
+			<div className='book-list'>
+				<ul>
+					<li>The Way Of Kings</li>
+					<li>The Name Of The Wind</li>
+					<li>The Final Empire</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Booklist;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+	render() {
+		return (
+			<nav>
+				<h1>Context App</h1>
+				<ul>
+					<li>About</li>
+					<li>Home</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		);
+	}
+}
+
+export default Navbar;
