2d0ce32a0a93d639ef515df40f7942f5078d16d9
meghnavarma0 <meghnavarma0@gmail.com>
2020-06-20 20:10:08 +0530

    styling for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
+	static contextType = ThemeContext;
 	render() {
+		console.log(this.context);
+		const { isLightTheme, light, dark } = this.context;
+		const theme = isLightTheme ? light : dark;
+		const { ui, syntax } = theme;
 		return (
-			<nav>
+			<nav style={{ background: ui, color: syntax }}>
 				<h1>Context App</h1>
 				<ul>
 					<li>About</li>
