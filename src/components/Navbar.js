464e614d6a58fe3446bb4a0d0a02b73e89f3d151
meghnavarma0 <meghnavarma0@gmail.com>
2020-06-21 10:46:00 +0530

    Added auth context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
 	render() {
 		return (
-			<ThemeContext.Consumer>
-				{context => {
-					const { isLightTheme, light, dark } = context;
-					const theme = isLightTheme ? light : dark;
-					const { ui, syntax } = theme;
+			<AuthContext.Consumer>
+				{authContext => (
+					<ThemeContext.Consumer>
+						{themeContext => {
+							const { isLightTheme, light, dark } = themeContext;
+							const theme = isLightTheme ? light : dark;
+							const { ui, syntax } = theme;
 
-					return (
-						<nav style={{ background: ui, color: syntax }}>
-							<h1>Context App</h1>
-							<ul>
-								<li>About</li>
-								<li>Home</li>
-								<li>Contact</li>
-							</ul>
-						</nav>
-					);
-				}}
-			</ThemeContext.Consumer>
+							const { toggleAuth, isAuthenticated } = authContext;
+
+							return (
+								<nav style={{ background: ui, color: syntax }}>
+									<h1>Context App</h1>
+									<div onClick={toggleAuth}>
+										{isAuthenticated
+											? 'Logged In'
+											: 'Logged Out'}
+									</div>
+									<ul>
+										<li>About</li>
+										<li>Home</li>
+										<li>Contact</li>
+									</ul>
+								</nav>
+							);
+						}}
+					</ThemeContext.Consumer>
+				)}
+			</AuthContext.Consumer>
 		);
 	}
 }
