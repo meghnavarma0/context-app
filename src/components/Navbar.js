6675d9affed1ed558059c9d48c151e376a42f38e
meghnavarma0 <meghnavarma0@gmail.com>
2020-06-22 14:33:14 +0530

    navbar changed to functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,26 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
-class Navbar extends Component {
-	render() {
-		return (
-			<AuthContext.Consumer>
-				{authContext => (
-					<ThemeContext.Consumer>
-						{themeContext => {
-							const { isLightTheme, light, dark } = themeContext;
-							const theme = isLightTheme ? light : dark;
-							const { ui, syntax } = theme;
+const Navbar = () => {
+	const { isLightTheme, light, dark } = useContext(ThemeContext);
+	const theme = isLightTheme ? light : dark;
+	const { ui, syntax } = theme;
 
-							const { toggleAuth, isAuthenticated } = authContext;
-
-							return (
-								<nav style={{ background: ui, color: syntax }}>
-									<h1>Context App</h1>
-									<div onClick={toggleAuth}>
-										{isAuthenticated
-											? 'Logged In'
-											: 'Logged Out'}
-									</div>
-									<ul>
-										<li>About</li>
-										<li>Home</li>
-										<li>Contact</li>
-									</ul>
-								</nav>
-							);
-						}}
-					</ThemeContext.Consumer>
-				)}
-			</AuthContext.Consumer>
-		);
-	}
-}
+	const { toggleAuth, isAuthenticated } = useContext(AuthContext);
+	return (
+		<nav style={{ background: ui, color: syntax }}>
+			<h1>Context App</h1>
+			<div onClick={toggleAuth}>
+				{isAuthenticated ? 'Logged In' : 'Logged Out'}
+			</div>
+			<ul>
+				<li>About</li>
+				<li>Home</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default Navbar;
