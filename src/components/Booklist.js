aac2aa33fdca7589818e032782ba81bccf7a53ba
meghnavarma0 <meghnavarma0@gmail.com>
2020-06-22 14:25:52 +0530

    booklist cahnged to functional component

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class Booklist extends Component {
-	static contextType = ThemeContext;
-	render() {
-		const { isLightTheme, light, dark } = this.context;
-		const theme = isLightTheme ? light : dark;
-		const { ui, syntax, bg } = theme;
-		return (
-			<div
-				className='book-list'
-				style={{ color: syntax, background: bg }}
-			>
-				<ul>
-					<li style={{ background: ui }}>The Way Of Kings</li>
-					<li style={{ background: ui }}>The Name Of The Wind</li>
-					<li style={{ background: ui }}>The Final Empire</li>
-				</ul>
-			</div>
-		);
-	}
-}
+const Booklist = () => {
+	const { isLightTheme, light, dark } = useContext(ThemeContext);
+	const theme = isLightTheme ? light : dark;
+	const { ui, syntax, bg } = theme;
+	return (
+		<div className='book-list' style={{ color: syntax, background: bg }}>
+			<ul>
+				<li style={{ background: ui }}>The Way Of Kings</li>
+				<li style={{ background: ui }}>The Name Of The Wind</li>
+				<li style={{ background: ui }}>The Final Empire</li>
+			</ul>
+		</div>
+	);
+};
 
 export default Booklist;
